d01eef184ede0aa3e837985008d133b99d83660e
bapeoriginals <68031565+bapeoriginals@users.noreply.github.com>
2020-07-09 02:58:21 +0200

    Create Main.php

---
+++ src/BorderX/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BorderX;
+
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase as Plugin;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\utils\Config;
+
+class Main extends Plugin implements Listener {
+
+ public function onEnable() {
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+	$this->saveDefaultConfig();
+ }
+ 
+ public function onMove(PlayerMoveEvent $event) {
+  $player = $event->getPlayer();
+  $levelname = $this->getConfig()->get("LevelName");
+  $level = $this->getServer()->getLevelByName($levelname);
+  $distance = $level->getSpawnLocation()->distance($player);
+   if($distance >= $this->getConfig()->get("Size")) {
+    $event->setCancelled(true);
+    $player->sendMessage($this->getConfig()->get("BorderMSG"));
+   }
+  }
+}
